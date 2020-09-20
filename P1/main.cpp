821d7dcbd7f607b82bc21c05f80d32aabfd34567
AlwynZ <soaragw2@gmail.com>
2020-09-20 02:35:54 -0400

    project 1 progress

--- P1/main.cpp
+++ P1/main.cpp
@@ -4,15 +4,55 @@
 #include <GL/glew.h>
 #endif
 
+#define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include "vector"
+#include "iostream"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/mat4x4.hpp"
+#include "ShaderProgram.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
-int main(int argc, char* argv[]) {
+ShaderProgram program;
+
+float ratthewX = 0.0f;
+float ratthewY = 0.0f;
+float ratthewRotation;
+GLuint ratthewTexture;
+std::vector<glm::mat4> ratthewMatrix;
+
+float ratrickX = -4.0f;
+float ratrickY = 0.5f;
+GLuint ratrickTexture;
+glm::mat4 ratrickMatrix;
+
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+
+GLuint loadTexture(const char* filePath) {
+	int w, h, n;
+	unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+	GLuint textureID; glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	stbi_image_free(image);
+
+	return textureID;
+}
+
+
+void Initialize() {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("Project 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 
@@ -20,20 +60,116 @@ int main(int argc, char* argv[]) {
 	glewInit();
 #endif
 
+	glViewport(0, 0, 640, 480);
+
+	program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+
+	viewMatrix = glm::mat4(1.0f);
+	projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+	program.SetProjectionMatrix(projectionMatrix);
+	program.SetViewMatrix(viewMatrix);
+
+	glUseProgram(program.programID);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-	SDL_Event event;
-	while (gameIsRunning) {
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-				gameIsRunning = false;
-			}
-		}
+	for (int i = 0; i < 12; ++i) {
+		glm::mat4 matrix = glm::mat4(1.0f);
+		matrix = glm::translate(matrix, glm::vec3(-5 + 1 * i, 0.2f, 0.0f));
+		matrix = glm::scale(matrix, glm::vec3(1.0f, 1.0f, 0.0f));
 
-		glClear(GL_COLOR_BUFFER_BIT);
-		SDL_GL_SwapWindow(displayWindow);
+		ratthewMatrix.push_back(matrix);
 	}
 
+	ratthewTexture = loadTexture("ratthew.png");
+	
+	ratrickMatrix = glm::mat4(1.0f);
+	ratrickTexture = loadTexture("ratrick.jpg");
+}
+
+
+void ProcessInput() {
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+			gameIsRunning = false;
+		}
+	}
+}
+
+
+float lastTicks = 0.0f;
+
+void Update() {
+	float ticks = (float)SDL_GetTicks() / 1000.0f;
+	float deltaTime = ticks - lastTicks;
+	lastTicks = ticks;
+
+	ratrickMatrix = glm::mat4(1.0f);
+
+	ratthewRotation = 90 * deltaTime;
+	for (glm::mat4& matrix : ratthewMatrix) {
+		matrix = glm::rotate(matrix, glm::radians(ratthewRotation), glm::vec3(0.0f, 0.0f, 1.0f));
+	}
+
+	if (ratrickX <= 4.0f) {
+		ratrickY += -0.1 * deltaTime;
+		ratrickX += 1 * deltaTime;
+	}
+	else {
+		ratrickY += 0.1 * deltaTime;
+		ratrickX += -1 * deltaTime;
+	}
+
+	ratrickMatrix = glm::translate(ratrickMatrix, glm::vec3(ratrickX, ratrickY, 0.0f));
+}
+
+
+void Render() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+	float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+	glEnableVertexAttribArray(program.positionAttribute);
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	for (const glm::mat4& rat : ratthewMatrix) {
+		program.SetModelMatrix(rat);
+		glBindTexture(GL_TEXTURE_2D, ratthewTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+	}
+
+	program.SetModelMatrix(ratrickMatrix);
+	glBindTexture(GL_TEXTURE_2D, ratrickTexture);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+	glDisableVertexAttribArray(program.positionAttribute);
+	glDisableVertexAttribArray(program.texCoordAttribute);
+
+
+	SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
 	SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+	Initialize();
+
+	while (gameIsRunning) {
+		ProcessInput();
+		Update();
+		Render();
+	}
+
+	Shutdown();
 	return 0;
 }
